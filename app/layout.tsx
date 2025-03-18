2a499715b74edc49bcc0c4b30264dc82d9ee2173
summer0512 <summer051200@gmail.com>
2025-03-18 18:00:34 +0800

    google-analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import {ReactNode} from 'react';
+import Script from 'next/script';
 
 type Props = {
   children: ReactNode;
@@ -7,5 +8,25 @@ type Props = {
 // Since we have a `not-found.tsx` page on the root, a layout file
 // is required, even if it's just passing children through.
 export default function RootLayout({children}: Props) {
-  return children;
+  return (
+    <>
+      <Script
+        strategy="afterInteractive"
+        src="https://www.googletagmanager.com/gtag/js?id=G-DJ2XMKT62S"
+      />
+      <Script
+        id="google-analytics"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-DJ2XMKT62S');
+          `,
+        }}
+      />
+      {children}
+    </>
+  );
 }
