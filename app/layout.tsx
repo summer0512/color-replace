eaa1b433b37370c5f8e5b1674a28e5e7ab4bbbfe
summer0512 <summer051200@gmail.com>
2025-09-18 19:14:20 +0800

    feat: adsense

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,11 +12,16 @@ export default function RootLayout({children}: Props) {
     <>
       <Script
         strategy="afterInteractive"
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2623631636848395"
+        crossOrigin="anonymous"
+      />
+      <Script
+        strategy="lazyOnload"
         src="https://www.googletagmanager.com/gtag/js?id=G-DJ2XMKT62S"
       />
       <Script
         id="google-analytics"
-        strategy="afterInteractive"
+        strategy="lazyOnload"
         dangerouslySetInnerHTML={{
           __html: `
             window.dataLayer = window.dataLayer || [];
